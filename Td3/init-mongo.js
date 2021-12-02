475ce0301ca1d87d61e6d9074eb9daee4a8af2e6
BLOT Tristan <tristan.blot3@etu.univ-lorraine.fr>
2021-12-02 12:19:15 +0100

    ajout données script

--- Td3/init-mongo.js
+++ Td3/init-mongo.js
@@ -10,3 +10,33 @@ db.createUser (
         ]
     }
 )
+
+db.createCollection('commune');
+db.createColletion('stop');
+
+db.commune.insertMany([
+  {
+    nom: 'Vandoeuvre',
+  },
+  {
+    nom: 'Villiers',
+  },
+  {
+    nom: 'Laxou',
+  }  
+]);
+
+db.stop.insertMany([
+  {
+    commune: 'Vandoeuvre',
+    position: '48.658518,6.269140',
+  },
+  {
+    commune: 'Laxou',
+    position: '48.658518,6.269140',
+  },
+  {
+    commune: 'Villiers',
+    position: '48.658518,6.269140',
+  }  
+]);
