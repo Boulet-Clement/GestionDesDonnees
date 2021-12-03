136d5cb0b9bb172a8463eb18ca106c215932050d
Clément Boulet <clement.boulet60510@gmail.com>
2021-12-03 08:09:59 +0100

    Update connexion.php

--- Td3/connexion.php
+++ Td3/connexion.php
@@ -1,3 +1,13 @@
-<?php 
+<?php /*
 $manager = new MongoDB\Driver\Manager("mongodb://cboulet:motdepasse1@localhost:27017");
 var_dump($manager);
+
+   /* connect to mongodb
+   $m = new MongoDB\Client();
+	
+   echo "Connection to database successfully";
+   // select a database
+   $db = $m->commune;
+	
+   echo "Database mydb selected";*/
+phpinfo();
