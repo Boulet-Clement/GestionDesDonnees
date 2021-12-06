c461f39f0206e6f80ba27f3ad1766205882278fb
Clément Boulet <clement.boulet60510@gmail.com>
2021-12-06 11:49:55 +0100

    Update index.php

--- Td3/public/index.php
+++ Td3/public/index.php
@@ -4,9 +4,7 @@ require_once "../src/vendor/autoload.php" ;
 
 $client = new MongoDB\Client("mongodb://mongo");
 
-$collection = $client->td3->beers;
-
-$result = $collection->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
+$collection = $client->td3->parkings;
 
 $response = file_get_contents('https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson');
 var_dump($response);
@@ -15,9 +13,3 @@ $json = json_decode($response);
 foreach ($json->{'features'} as $item) {
     $collection->insertOne($item);
  }
-
-/*
-echo "Inserted with Object ID '{$result->getInsertedId()}'";
-
-*/
-//curl php curl
