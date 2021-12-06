57b1c5863a80f3b25d9ae09002c455be7fdf4e31
Clément Boulet <clement.boulet60510@gmail.com>
2021-12-06 12:01:56 +0100

    Update index.php

--- Td3/public/index.php
+++ Td3/public/index.php
@@ -7,9 +7,16 @@ $client = new MongoDB\Client("mongodb://mongo");
 $collection = $client->td3->parkings;
 
 $response = file_get_contents('https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson');
-var_dump($response);
+//var_dump($response);
 $json = json_decode($response);
 
-foreach ($json->{'features'} as $item) {
+/*foreach ($json->{'features'} as $item) {
     $collection->insertOne($item);
- }
+}*/
+
+$cursor = $collection->find();
+   // iterate cursor to display title of documents
+	
+   foreach ($cursor as $document) {
+        echo($document->{'attributes'}->{'NOM'}."<br>");
+   }
