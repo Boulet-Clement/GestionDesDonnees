9a764f4d5393a77de23d690e017f241116b17891
Clément Boulet <clement.boulet60510@gmail.com>
2022-02-01 11:23:38 +0100

    Si les parkings ne sont pas en base de donnée, on va les chercher

--- Td3/public/index.php
+++ Td3/public/index.php
@@ -6,26 +6,17 @@ $client = new MongoDB\Client("mongodb://mongo");
 
 $collection = $client->td3->parkings;
 
-$response = file_get_contents('https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson');
-//var_dump($response);
-$json = json_decode($response);
-
-/*foreach ($json->{'features'} as $item) {
-    $collection->insertOne($item);
-}*/
-
 $cursor = $collection->find();
-   // iterate cursor to display title of documents
-  // file_put_contents("test.json",$cursor);
+$parkingJSON = json_encode(iterator_to_array($cursor));
 
-    /*$myfile = fopen("test.json", "w") or die("Unable to open file!");
-    fwrite($myfile,"{\"parkings\":[");
-    foreach ($cursor as $document) {
-        fwrite($myfile,json_encode($document));
-        fwrite($myfile,",");
-        //echo($document->{'attributes'}->{'NOM'}."<br>");
-        //file_put_contents("test.json",$document);
-    }
-    //ftruncate($myfile, rand(1, filesize("test.json")-1));
-    fwrite($myfile,"]}");
-    fclose($myfile);*/
+if ($parkingJSON === "[]"){
+  $response = file_get_contents('https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson');
+  $json = json_decode($response);
+  foreach ($json->{'features'} as $item) {
+    $collection->insertOne($item);
+  }
+  $cursor = $collection->find();
+  $parkingJSON = json_encode(iterator_to_array($cursor));
+}
+
+
