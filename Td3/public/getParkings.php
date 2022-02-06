6033ac2686cc6f92d298581403cabb72d0ec8a73
Clément Boulet <clement.boulet60510@gmail.com>
2022-02-06 14:20:39 +0100

    Commentaire

--- Td3/public/getParkings.php
+++ Td3/public/getParkings.php
@@ -9,6 +9,7 @@ $collection = $client->td3->parkings;
 $cursor = $collection->find();
 $parkingJSON = json_encode(iterator_to_array($cursor));
 
+//Si on récupère quelque chose de Vide, on va chercher les données et on les insert en base de données
 if ($parkingJSON === "[]"){
   $response = file_get_contents('https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson');
   $json = json_decode($response);
