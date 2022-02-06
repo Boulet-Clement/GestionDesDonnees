846a8bcbd701dcdd3bd9dac41cdf63d4c5d360b4
Clément Boulet <clement.boulet60510@gmail.com>
2022-02-06 14:49:06 +0100

    suppresion des console.log

--- Td3/public/script.js
+++ Td3/public/script.js
@@ -1,3 +1,13 @@
+// "MAIN"
+
+const lattitude = 48.692114
+const longitude = 6.184279
+document.addEventListener("DOMContentLoaded", async function () {
+    displayMap(lattitude ,longitude);
+    addYouAreHere()
+    getParkings()
+});
+
 let map;
 
 //Affichage de la carte sur Nancy
@@ -11,80 +21,6 @@ function displayMap(lat, lon) {
     });
 }
 
-//Fonction d'ajout d'un marker
-function addParkingMarker(lat, long, parking) {
-
-    const popup_logo = document.createElement('div');
-        popup_logo.style.width = `40px`;
-        popup_logo.style.height = `60px`;
-        popup_logo.style.backgroundSize = '100%';
-        popup_logo.style.backgroundRepeat = 'no-repeat'
-        popup_logo.style.backgroundImage = 'url(./assets/images/1072465.png)';
-        const popup_div = document.createElement("div");
-    const popup_title = document.createElement("h1");
-        popup_title.innerText = parking.attributes.NOM
-    const popup_description = document.createElement("p");
-        popup_description.innerHTML = `capacité : ${parking.attributes.CAPACITE}
-            <br/> places: ${parking.attributes.PLACES}`
-        
-    const popup_button = document.createElement("button");
-        popup_button.setAttribute("id","test")
-        popup_button.innerText="voir les commentaires";
-        popup_button.addEventListener("click", function(){
-            displayComments(parking)
-            addInputForComment(parking)
-            console.log("voir les commentaires")
-        })
-        popup_div.appendChild(popup_title);
-        popup_div.appendChild(popup_description);
-        popup_div.appendChild(popup_button);
-    const popup = new mapboxgl.Popup({ offset: 25 }).setDOMContent(
-        popup_div
-    );
-    new mapboxgl.Marker(popup_logo).setLngLat([long, lat]).setPopup(popup).addTo(map);
-}
-
-// Ajout de l'input pour les commentaires
-function addInputForComment(parking){
-    console.log(parking)
-    const formComment = document.createElement('form');
-        formComment.method='post'
-        formComment.name="form_comment"
-        formComment.action="/postComment.php"
-    const inputComment = document.createElement('input')
-        inputComment.name="input_comment"
-        inputComment.setAttribute("id", "input_comments")
-        inputComment.setAttribute("type", "text")
-    const inputID = document.createElement('input')
-        inputID.name="input_id"
-        inputID.setAttribute('id', "input_id")
-        inputID.setAttribute("type", "hidden")
-        inputID.value=parking._id.$oid
-    const buttonSubmit = document.createElement('button')
-        buttonSubmit.innerText="Envoyer"
-        buttonSubmit.setAttribute("type", "submit")
-    const div_comments = document.getElementById('comments')
-    div_comments.innerHTML=""
-    div_comments.appendChild(formComment)
-    formComment.appendChild(inputComment)
-    formComment.appendChild(inputID)
-    formComment.appendChild(buttonSubmit)
-}
-
-// Affichage des commentaires
-function displayComments(parking){
-    console.log("aaah")
-    const divComments = document.getElementById('div_comments')
-        divComments.innerHTML=""
-        console.log(parking.comments)
-    if (parking.comments){
-        parking.comments.forEach(comment => {
-            const p_comment = document.createElement("p");
-            p_comment.innerText = comment
-            divComments.appendChild(p_comment)
-        });
-    }
-}
 //Ajout du marker rouge "vous êtes ici"
 async function addYouAreHere() {
     const lat = 48.69211422805919;
@@ -111,7 +47,6 @@ async function getParkings() {
     // les données arrivent correctement
     xhr.addEventListener("load", function (response) {
         addParkings(response.target.response);
-        //addEventClicks(response.target.response);
     });
     // on a un code d'erreur
     xhr.addEventListener("error", function (response) {
@@ -121,28 +56,92 @@ async function getParkings() {
 
 // Ajout des parkings à la carte
 async function addParkings(parkings) {
-
     parkings.forEach(parking => {
-        console.log(parking)
-        addParkingMarker(parking.geometry.y, parking.geometry.x, parking)
+        addParkingMarker(parking)
     });
+}
+
+//Fonction d'ajout d'un marker
+function addParkingMarker(parking) {
+    const popup_logo = document.createElement('div');
+        popup_logo.style.width = `40px`;
+        popup_logo.style.height = `60px`;
+        popup_logo.style.backgroundSize = '100%';
+        popup_logo.style.backgroundRepeat = 'no-repeat'
+        popup_logo.style.backgroundImage = 'url(./assets/images/1072465.png)';
+    
+
+    const popup_title = document.createElement("h1");
+        popup_title.innerText = parking.attributes.NOM
+
+    const popup_description = document.createElement("p");
+        popup_description.innerHTML = `capacité : ${parking.attributes.CAPACITE}
+            <br/> places: ${parking.attributes.PLACES}`
+        
+    const popup_button = document.createElement("button");
+        popup_button.setAttribute("id","test")
+        popup_button.innerText="voir les commentaires";
+        popup_button.addEventListener("click", function(){
+            displayComments(parking)
+            addInputForComment(parking)
+        })
+
+    const popup_div = document.createElement("div");
+        popup_div.appendChild(popup_title);
+        popup_div.appendChild(popup_description);
+        popup_div.appendChild(popup_button);
+
+    const popup = new mapboxgl.Popup({ offset: 25 }).setDOMContent(
+        popup_div
+    );
+    
+    new mapboxgl.Marker(popup_logo).setLngLat([parking.geometry.x, parking.geometry.y]).setPopup(popup).addTo(map);
 }
 
 // Ajout des eventListener sur les markers
 async function addEventClicks(parkings) {
-
     parkings.forEach(parking => {
-        console.log(parking)
         const button = document.getElementById(`btn-${parking._id.$oid}`)
-        console.log(`btn-${parking._id.$oid}`)
         button.addEventListener("click", function(){
-            console.log("you clicked")
         })
     })
 }
-// "MAIN"
-document.addEventListener("DOMContentLoaded", async function () {
-    displayMap(48.692114, 6.184279);
-    addYouAreHere()
-    getParkings()
-});
+
+// Ajout de l'input pour les commentaires
+function addInputForComment(parking){
+    const formComment = document.createElement('form');
+        formComment.method='post'
+        formComment.name="form_comment"
+        formComment.action="/postComment.php"
+    const inputComment = document.createElement('input')
+        inputComment.name="input_comment"
+        inputComment.setAttribute("id", "input_comments")
+        inputComment.setAttribute("type", "text")
+    const inputID = document.createElement('input')
+        inputID.name="input_id"
+        inputID.setAttribute('id', "input_id")
+        inputID.setAttribute("type", "hidden")
+        inputID.value=parking._id.$oid
+    const buttonSubmit = document.createElement('button')
+        buttonSubmit.innerText="Envoyer"
+        buttonSubmit.setAttribute("type", "submit")
+    const div_comments = document.getElementById('div_input_comments')
+    div_comments.innerHTML=""
+    div_comments.appendChild(formComment)
+    formComment.appendChild(inputComment)
+    formComment.appendChild(inputID)
+    formComment.appendChild(buttonSubmit)
+}
+
+// Affichage des commentaires
+function displayComments(parking){
+    const divComments = document.getElementById('div_comments')
+        divComments.innerHTML=""
+    if (parking.comments){
+        parking.comments.forEach(comment => {
+            const p_comment = document.createElement("p");
+            p_comment.innerText = comment
+            divComments.appendChild(p_comment)
+        });
+    }
+}
